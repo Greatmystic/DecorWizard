351d1b946c819c242f4209d06354c44eb7a37536
Greatmystic <supergreatmystic@gmail.com>
2024-08-17 14:09:55 -0400

    script added to Tilemap

--- Content/Scripts/Level.gd
+++ Content/Scripts/Level.gd
@@ -1,10 +1,23 @@
 extends TileMap
 
-# Called when the node enters the scene tree for the first time.
+var GridSize = 4
+var Dic = {}
+
 func _ready():
-	pass # Replace with function body.
-
-
+	for x in GridSize:
+		for y in GridSize:
+			Dic[str(Vector2(x,y))] ={
+			"Type" : "Grass"
+			}
+			set_cell(0, Vector2(x, y), 0, Vector2i(0,0), 0)
+			
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var tile = local_to_map(get_global_mouse_position())
+	
+	for x in GridSize:
+		for y in GridSize:
+			erase_cell(1, Vector2(x, y))
+			
+		if Dic.has(str(tile)):
+			set_cell(1, tile, 1, Vector2i(0,0), 0)
